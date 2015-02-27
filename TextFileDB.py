75725cd9b7d692243c151c667d7349318321d163
mra1385 <mikeazar@gmail.com>
2015-02-27 17:26:26 -0500

    DeletedTestCode
    Deleted unnecessary test code

--- TextFileDB.py
+++ TextFileDB.py
@@ -130,20 +130,3 @@ class Database(object):
 			lines = f.readlines()
 		with open(self.file_path,"w") as f:
 			[f.write(line) for line in lines if line.strip()]
-
-database1 = Database('DB1','text1.txt',',',['id','first_name', 'last_name'])
-entry1 = {'id': '1', 'first_name': 'Mike', 'last_name': 'Azar'}
-entry2 = {'id': '1', 'first_name': 'John', 'last_name': 'Smith'}
-entry3 = {'id': '3', 'first_name': 'Mike', 'last_name': 'Twain'}
-entry4 = {'id': '4', 'first_name': 'Tim', 'last_name': 'Valbuena'}
-entry5 = {'id': '5', 'first_name': 'Joey', 'last_name': 'Mustafa'}
-entry6 = {'id': '6', 'first_name': 'Tom', 'last_name': 'Jerry'}
-
-s = {'id': '1'}
-r = {'last_name': 'HMAR'}
-
-database1.add_data(entry1,entry2,entry3,entry4,entry5,entry6)
-database1.read_db()
-database1.update(s,r,True)
-database1.read_db()
-
